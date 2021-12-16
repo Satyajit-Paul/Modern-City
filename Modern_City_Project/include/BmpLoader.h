e97b57529bf6bab7092955616b4f978e6b4aeba0
Satyajit Paul <virusjit7@gmail.com>
2021-12-16 10:48:05 +0600

    This is the final message git add Modern_City_Project!

---
+++ Modern_City_Project/include/BmpLoader.h
@@ -0,0 +1,22 @@
+#ifndef BMPLOADER_H
+#define BMPLOADER_H
+#include<windows.h>
+
+
+class BmpLoader
+{
+    public:
+        unsigned char *textureData;
+        int iWidth, iHeight;
+
+        BmpLoader(const char*);
+        ~BmpLoader();
+
+    protected:
+
+    private:
+        BITMAPFILEHEADER bfh;
+        BITMAPINFOHEADER bih;
+};
+
+#endif // BMPLOADER_H
